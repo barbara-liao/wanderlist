b294cb385d31c55d92bbf36306f258f5110dd027
Barbara Liao <barbara.liao3@gmail.com>
2021-11-17 17:17:17 -0800

    Updated date parser.

--- client/lib/parse-date.js
+++ client/lib/parse-date.js
@@ -1,7 +1,5 @@
 export default function parseDate(rawDate) {
-  const dateString = new Date(rawDate).toString();
-  const dateArr = dateString.split(' ');
-  const [day, month, date, year] = dateArr;
-  const formattedDate = `${day}, ${month} ${date}, ${year}`;
-  return formattedDate;
+  const newDate = new Date(rawDate);
+  const dateString = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(newDate);
+  return dateString;
 }
