408481a5c5dc4bc9f26ae8418472edbac827705f
Barbara Liao <barbara.liao3@gmail.com>
2021-11-17 18:06:31 -0800

    Updated date parser to include timezone.

--- client/lib/parse-date.js
+++ client/lib/parse-date.js
@@ -1,5 +1,11 @@
-export default function parseDate(rawDate) {
-  const newDate = new Date(rawDate);
-  const dateString = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(newDate);
-  return dateString;
+export default function parseDate(date) {
+  const newDate = new Date(date);
+  const dateString = new Intl.DateTimeFormat();
+  const locale = dateString.resolvedOptions().timeZone;
+  const options = {
+    dateStyle: 'full',
+    timeZone: locale
+  };
+  const dateString2 = new Intl.DateTimeFormat(options).format(newDate);
+  return dateString2;
 }
