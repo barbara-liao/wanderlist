c38d08c6db59e9af4fb2d550bd05dcfe342948fa
Barbara Liao <barbara.liao3@gmail.com>
2021-12-01 09:51:54 -0800

    Added time zone difference to the date.

--- client/lib/parse-date.js
+++ client/lib/parse-date.js
@@ -1,11 +1,9 @@
-export default function parseDate(date) {
-  const newDate = new Date(date);
-  const dateString = new Intl.DateTimeFormat();
-  const locale = dateString.resolvedOptions().timeZone;
-  const options = {
-    dateStyle: 'full',
-    timeZone: locale
-  };
-  const dateString2 = new Intl.DateTimeFormat(options).format(newDate);
-  return dateString2;
+export default function parseDate(rawDate) {
+  const dt = new Date(rawDate);
+  dt.setMinutes(dt.getMinutes() + dt.getTimezoneOffset());
+  const dateString = dt.toString();
+  const dateArr = dateString.split(' ');
+  const [day, month, date, year] = dateArr;
+  const formattedDate = `${day}, ${month} ${date}, ${year}`;
+  return formattedDate;
 }
